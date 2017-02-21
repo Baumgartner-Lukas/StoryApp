509b53f842f550816b954eba95741576c7030869
WebTree-Baumgartner <baumgartner.webtree@gmail.com>
2017-02-21 14:49:02 +0100

    Story App: Last Chapter @ 5th video

--- app/src/main/java/com/webtree/baumgartner/storyapp/model/Story.java
+++ app/src/main/java/com/webtree/baumgartner/storyapp/model/Story.java
@@ -59,4 +59,8 @@ public class Story {
                         "you forever wonder what was sending that signal." +
                         " Perhaps a future mission will be able to investigate...");
     }
+
+    public Page getPage(int pageNumber){
+        return mPages[pageNumber];
+    }
 }
--- app/src/main/java/com/webtree/baumgartner/storyapp/ui/StoryActivity.java
+++ app/src/main/java/com/webtree/baumgartner/storyapp/ui/StoryActivity.java
@@ -1,12 +1,19 @@
 package com.webtree.baumgartner.storyapp.ui;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.webtree.baumgartner.storyapp.R;
 import com.webtree.baumgartner.storyapp.model.Page;
+import com.webtree.baumgartner.storyapp.model.Story;
+
+import static android.R.attr.name;
 
 public class StoryActivity extends AppCompatActivity {
 
@@ -14,21 +21,52 @@ public class StoryActivity extends AppCompatActivity {
 
     public static final String TAG = StoryActivity.class.getSimpleName();
 
+    private Story mStory = new Story();
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mChoice1;
+    private Button mChoice2;
+    private String mName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
+        mName = intent.getStringExtra(getString(R.string.key_name));
 
-        if (name == null) {
-            name = "Friend";
+        if (mName == null) {
+            mName = "Friend";
         }
 
-        Log.d(TAG, name);
+        Log.d(TAG, mName);
 
+        mImageView = (ImageView)findViewById(R.id.storyImageView);
+        mTextView = (TextView) findViewById(R.id.storyTextView);
+        mChoice1 = (Button) findViewById(R.id.choiceButton1);
+        mChoice2 = (Button) findViewById(R.id.choiceButton2);
 
+        loadPage();
+    }
+
+    private void loadPage(){
+        Page page = mStory.getPage(0);
+
+        Drawable drawable = getResources().getDrawable(page.getImageId());
+        mImageView.setImageDrawable(drawable);
+        //Add the name if placeholder is included, won't add if no placeholder
+        String pageText = page.getText();
+        pageText = String.format(pageText, mName);
+        mTextView.setText(pageText);
+        mChoice1.setText(page.getChoice1().getText());
+        mChoice2.setText(page.getChoice2().getText());
 
     }
 }
+
+
+
+
+
+
